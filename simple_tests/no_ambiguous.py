941e78f4268819ea877ccdd538714c700da51c20
evandrocoan <evandrocoan@hotmail.com>
2018-07-06 15:31:56 -0300

    Added digit and quote support for no_ambiguous.py

--- simple_tests/no_ambiguous.py
+++ simple_tests/no_ambiguous.py
@@ -12,13 +12,13 @@ _parser = lark.Lark( r"""
 
         production_symbols   : terminals_begin | non_terminals_begin
         terminal_symbols     : LOW_CASE_LETTER
-        non_terminal_symbols : UPPER_CASE_LETTER
+        non_terminal_symbols : UPPER_CASE_LETTER | QUOTE | DIGIT
 
         non_terminals_begin : non_terminal_symbols non_terminals_end
-        non_terminals_end   : non_terminal_symbols non_terminals_end | white_space+ | UPPER_CASE_LETTER2
+        non_terminals_end   : non_terminal_symbols non_terminals_end | WHITE_SPACE+ | UPPER_CASE_LETTER2 | QUOTE2 | DIGIT2
 
         terminals_begin : terminal_symbols terminals_end
-        terminals_end   : terminal_symbols terminals_end | white_space+ | LOW_CASE_LETTER2
+        terminals_end   : terminal_symbols terminals_end | WHITE_SPACE+ | LOW_CASE_LETTER2
 
         LOW_CASE_LETTER  : /[a-zØ-öø-ÿ]/
         LOW_CASE_LETTER2 : /[a-zØ-öø-ÿ]$/
@@ -26,12 +26,21 @@ _parser = lark.Lark( r"""
         UPPER_CASE_LETTER  : /[A-ZÀ-Ö]/
         UPPER_CASE_LETTER2 : /[A-ZÀ-Ö]$/
 
+        QUOTE  : "'"
+        QUOTE2 : /'$/
+
+        DIGIT  : "0".."9"
+        DIGIT2 : /[0-9]$/
+
+        CR        : /\r/
+        LF        : /\n/
+        NEWLINE   : (CR? LF)+
+
         // Common definitions
         WHITE_SPACE : ( " " | /\t/ )
-        ?white_space : WHITE_SPACE
 """, start='grammar', parser='lalr' )
 # """, start='grammar', parser='earley', ambiguity="explicit" )
 
-tree = _parser.parse( "S -> a | S SS AAA  |  B  |  C  " )
+tree = _parser.parse( "S -> a 1 | aa S1 a SS AAA  |  B  |  C  " )
 print( tree.pretty() )
 
