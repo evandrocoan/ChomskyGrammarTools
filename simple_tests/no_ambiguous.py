d45699e2fc1d04d184dcba28dd6d236d3e5f3a9d
evandrocoan <evandrocoan@hotmail.com>
2018-07-06 20:39:29 -0300

    Reimplemented the no_ambiguous.py grammar only with 3 levels depth
    on its generated tree

--- simple_tests/no_ambiguous.py
+++ simple_tests/no_ambiguous.py
@@ -4,50 +4,30 @@
 import lark
 
 _parser = lark.Lark( r"""
-        productions : start_symbol "->" production_symbols
-
-        production_symbols : WHITE_SPACE* token+ "|" production_symbols
-                           | WHITE_SPACE* token+
+        grammar : ( start_symbol "->" productions new_line+ )* ( start_symbol "->" productions )
 
         // Rename the start symbol, so when parsing the tree it is simple to find it
-        start_symbol : WHITE_SPACE* token+
+        start_symbol : tokens
+        productions  : tokens ( "|" tokens )*
 
-        token                : terminal_begin | non_terminal_begin
-        terminal_symbols     : LOW_CASE_LETTER | DIGIT | EPSILON | signs | signs_extra | parens
-        non_terminal_symbols : UPPER_CASE_LETTER | DIGIT | QUOTE
+        token  : terminals | non_terminals
+        tokens : SPACES* ( token+ SPACES+ )* token*
 
-        terminal_begin : terminal_symbols terminal_end
-        terminal_end   : terminal_symbols terminal_end
-                        | LOW_CASE_LETTER2
-                        | WHITE_SPACE+
-                        | WHITE_SPACE+ new_line productions
-                        | new_line productions
-
-        non_terminal_begin : UPPER_CASE_LETTER non_terminal_end
-        non_terminal_end   : non_terminal_symbols non_terminal_end
-                            | QUOTE2
-                            | DIGIT2
-                            | UPPER_CASE_LETTER2
-                            | WHITE_SPACE+
-                            | WHITE_SPACE+ new_line productions
-                            | new_line productions
+        terminals     : ( LOWER_CASE_LETTER | EPSILON | DIGIT | signs | signs_extra | parens )+
+        non_terminals : UPPER_CASE_LETTER ( UPPER_CASE_LETTER | DIGIT | QUOTE )*
 
         // https://stackoverflow.com/questions/20690499/concrete-javascript-regex-for-accented-characters-diacritics
-        LOW_CASE_LETTER  : /[a-zØ-öø-ÿ]/
-        LOW_CASE_LETTER2 : /[a-zØ-öø-ÿ]$/
+        UPPER_CASE_LETTER : /[A-ZÀ-Ö]/
+        LOWER_CASE_LETTER : /[a-zØ-öø-ÿ]/
 
-        UPPER_CASE_LETTER  : /[A-ZÀ-Ö]/
-        UPPER_CASE_LETTER2 : /[A-ZÀ-Ö]$/
-
-        QUOTE  : "'"
-        QUOTE2 : /'$/
-
-        DIGIT  : "0".."9"
-        DIGIT2 : /[0-9]$/
-
-        CR        : /\r/
-        LF        : /\n/
-        new_line  : (CR? LF)+
+        // Common definitions
+        EPSILON  : "&"
+        QUOTE    : "'"
+        DIGIT    : "0".."9"
+        SPACES   : ( " " | /\t/ )
+        CR       : /\r/
+        LF       : /\n/
+        new_line : ( CR? LF )+
 
         // Tells the tree-builder to inline this branch if it has only one member
         ?signs    : MINUS |  PLUS | STAR | COMMA | COLON | EQUALS | SEMICOLON | SLASH | BACKSLASH | DOT
@@ -82,13 +62,11 @@ _parser = lark.Lark( r"""
         CLOSE_BRACE   : "}"
         OPEN_PAREN    : "("
         CLOSE_PAREN   : ")"
+""", start='grammar', parser='lalr' )
+# """, start='grammar', parser='earley', ambiguity="explicit" )
 
-        // Common definitions
-        EPSILON     : "&"+
-        WHITE_SPACE : ( " " | /\t/ )
-""", start='productions', parser='lalr' )
-# """, start='productions', parser='earley', ambiguity="explicit" )
-
-tree = _parser.parse( "S -> a & 1)\" | aa S1 a SS AAA  |  B  |  C  a\n B -> BB\nC -> CC" )
+tree = _parser.parse( "S -> S SS | &" )
+# tree = _parser.parse( "S -> a & 1)\" | aa S1 a SS AAA  |  B  |  C  a\nB -> BB\nC -> CC" )
+# tree = _parser.parse( "S SS -> AA BBBB | EEE\nC -> CC\nD-> DD DDD" )
 print( tree.pretty() )
 
