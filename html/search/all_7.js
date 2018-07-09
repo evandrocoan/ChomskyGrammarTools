b2282b1b6f0fc85216e68d6d3d9c2639f2694c12
evandrocoan <evandrocoan@hotmail.com>
2018-07-09 01:03:07 -0300

    Updated the documentation with the lasted changes on master

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['get_5ffactoring_5fduplicated_5felements',['get_factoring_duplicated_elements',['../classgrammar_1_1grammar_1_1_chomsky_grammar.html#a810a3bbcbe575068db2807d1a6c7480f',1,'grammar::grammar::ChomskyGrammar']]],
+  ['get_5fbiggest_5ffactor_5fserie',['get_biggest_factor_serie',['../classgrammar_1_1grammar_1_1_chomsky_grammar.html#adcd69da0d9f1d234fc14d5934a301f5d',1,'grammar::grammar::ChomskyGrammar']]],
+  ['get_5fduplicated_5ffactors',['get_duplicated_factors',['../classgrammar_1_1grammar_1_1_chomsky_grammar.html#a3cf87954802f5881368fbb3936a081ec',1,'grammar::grammar::ChomskyGrammar']]],
   ['get_5fiterator',['get_iterator',['../classgrammar_1_1dynamic__iteration_1_1_dynamic_iteration_dict.html#a396c8120e8d7ce8561e724bdbc0f5d52',1,'grammar::dynamic_iteration::DynamicIterationDict']]],
   ['get_5fkey',['get_key',['../classgrammar_1_1dynamic__iteration_1_1_dynamic_iteration_dict.html#a452c25f93d89e5f7982272e501aea514',1,'grammar::dynamic_iteration::DynamicIterationDict']]],
   ['get_5fkey_5fvalue',['get_key_value',['../classgrammar_1_1dynamic__iteration_1_1_dynamic_iteration_dict.html#a19d36126582766a23e1644b4aa783fc5',1,'grammar::dynamic_iteration::DynamicIterationDict']]],
--- html/search/functions_7.js
+++ html/search/functions_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
+  ['has_5fdirect_5ffactors',['has_direct_factors',['../classgrammar_1_1grammar_1_1_chomsky_grammar.html#a58987ef5e53941fa2d35bff8a23933ff',1,'grammar::grammar::ChomskyGrammar']]],
   ['has_5fduplicated_5ffactors',['has_duplicated_factors',['../classgrammar_1_1grammar_1_1_chomsky_grammar.html#a2c4caaadb1f478618c3d41050ebdcfea',1,'grammar::grammar::ChomskyGrammar']]],
-  ['has_5findirect_5ffactors',['has_indirect_factors',['../classgrammar_1_1grammar_1_1_chomsky_grammar.html#a74899e310de904db457b646ed9e0a37b',1,'grammar::grammar::ChomskyGrammar']]],
   ['has_5fleft_5frecursion',['has_left_recursion',['../classgrammar_1_1grammar_1_1_chomsky_grammar.html#aa91d337c1c62cd6625a400951bcf30bf',1,'grammar::grammar::ChomskyGrammar']]],
   ['has_5fproduction',['has_production',['../classgrammar_1_1grammar_1_1_chomsky_grammar.html#a831b963e2931277a3202b98de51e914d',1,'grammar::grammar::ChomskyGrammar']]],
   ['has_5frecursion_5fon_5fthe_5fnon_5fterminal',['has_recursion_on_the_non_terminal',['../classgrammar_1_1grammar_1_1_chomsky_grammar.html#abbe77507f09e79bbc087dc46640afcf6',1,'grammar::grammar::ChomskyGrammar']]],
